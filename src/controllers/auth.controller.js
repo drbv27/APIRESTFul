5456725508f741159004bd8d00bb5709e0789dd9
Diego <drbv27@gmail.com>
2021-11-15 11:27:07 -0500

    Encrypt password

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+import User from "../models/User";
+
+export const signUp = async (req, res) => {
+  const { username, email, password, roles } = req.body;
+
+  const newUser = new User({
+    username,
+    email,
+    password: await User.encryptPassword(password),
+  });
+
+  console.log(newUser);
+
+  res.json("signup");
+};
+export const signin = async (req, res) => {
+  res.json("signin");
+};
--- src/database.js
+++ src/database.js
@@ -4,7 +4,10 @@ const PASS = "E9G7gMi9QfE4ojEi";
 const DATAB = "companydb";
 const uri = `mongodb+srv://${USER}:${PASS}@cluster0.mzxff.mongodb.net/${DATAB}?retryWrites=true&w=majority`;
 mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB Is connected");
   })
